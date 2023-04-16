9dfdaab0120395d9ffd93264188086c85e25752c
Marco Urriola <marco.urriola@gmail.com>
2023-04-15 20:15:53 -0400

    Add decrypt function

--- script.js
+++ script.js
@@ -43,29 +43,11 @@ function encrypt() {
 }
 
 function decrypt() {
-  const txt = getTxt();
-  let result = "";
-  for (let index = 0; index < txt.length; index++) {
-    let char = txt.charAt(index);
-    switch (char) {
-      case "e":
-        result = result + e;
-        break;
-      case "i":
-        result = result + i;
-        break;
-      case "a":
-        result = result + a;
-        break;
-      case "o":
-        result = result + o;
-        break;
-      case "u":
-        result = result + u;
-        break;
-      default:
-        result = result + char;
-    }
-  }
-  writeTxt(result);
+  let txt = getTxt();
+  txt = txt.replaceAll(e, "e");
+  txt = txt.replaceAll(i, "i");
+  txt = txt.replaceAll(a, "a");
+  txt = txt.replaceAll(o, "o");
+  txt = txt.replaceAll(u, "u");
+  writeTxt(txt);
 }
